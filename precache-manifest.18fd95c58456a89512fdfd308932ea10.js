8ca50836eeba9332f1973fa8b594bae0fa786879
Michael Parry <crashvideoproductions@gmail.com>
2020-01-21 18:42:55 -0500

    initial commit

---
+++ precache-manifest.18fd95c58456a89512fdfd308932ea10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dda96afe3c6235f41917a98b6a2d7b0",
+    "url": "/chirpsat/index.html"
+  },
+  {
+    "revision": "8bd6a761e61ba4f71ee2",
+    "url": "/chirpsat/static/js/2.ed29634e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/chirpsat/static/js/2.ed29634e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2740b4814d46f34df55a",
+    "url": "/chirpsat/static/js/main.1130d922.chunk.js"
+  },
+  {
+    "revision": "005f6ffae681b4bf4b02",
+    "url": "/chirpsat/static/js/runtime-main.9274d6f7.js"
+  }
+]);
