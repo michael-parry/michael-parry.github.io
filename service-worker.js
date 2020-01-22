f9d4cc5521959078ece3c0f5f6499d1a32edae0d
Michael Parry <crashvideoproductions@gmail.com>
2020-01-21 19:17:35 -0500

    update package.json

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chirpsat/precache-manifest.18fd95c58456a89512fdfd308932ea10.js"
+  "/chirpsat/precache-manifest.0954b8afff909d6f0eb208374f44e3b6.js"
 );
 
 self.addEventListener('message', (event) => {
