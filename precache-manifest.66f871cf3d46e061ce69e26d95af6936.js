0f2b366cc2f5e1894258a8ff63ebf989bc8968ea
Michael Parry <crashvideoproductions@gmail.com>
2020-01-31 22:27:52 -0500

    Updates

---
+++ precache-manifest.66f871cf3d46e061ce69e26d95af6936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1187aa1fa97bf9e3e7fbd6bcb057b714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d8874f2d0e40cbe976",
+    "url": "/static/js/2.47f2cb72.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.47f2cb72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e185c38e269199da9c3d",
+    "url": "/static/js/main.5e637dce.chunk.js"
+  },
+  {
+    "revision": "1e96a47aac545a8a9497",
+    "url": "/static/js/runtime-main.47f0f737.js"
+  }
+]);
